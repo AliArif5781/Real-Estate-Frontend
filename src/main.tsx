0a7218bd081f0964812f05089e8efc88c1c91cdc
AliArif5781 <aliairf92@gmail.com>
2025-05-19 02:23:39 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/sass/main.scss";
+import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>
 );
