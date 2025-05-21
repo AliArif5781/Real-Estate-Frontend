c48e5138bf73269f26b75d8820f218d7a5d64aa9
AliArif5781 <aliairf92@gmail.com>
2025-05-22 00:53:14 +0500

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
 import { Signup } from "../pages/Signup";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
