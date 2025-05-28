22f1f85de5f88164a4bfc5eb26e18ce09addaba1
AliArif5781 <aliairf92@gmail.com>
2025-05-28 23:54:33 +0500

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
 import { Signup } from "../pages/Signup";
 import Login from "../pages/Login";
+import HeroSection from "../pages/HeroSection";
+import { Aboutus } from "../pages/Aboutus";
+import NotFoundPage from "../pages/404Page";
+import { UserProfile } from "../pages/UserProfile";
 
 export const router = createBrowserRouter([
   {
     path: "",
     element: <Layout />,
-    children: [],
+    children: [
+      {
+        path: "",
+        element: <HeroSection />,
+      },
+      {
+        path: "about",
+        element: <Aboutus />,
+      },
+    ],
   },
   {
     path: "/signup",
@@ -16,5 +29,13 @@ export const router = createBrowserRouter([
   {
     path: "login",
     element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+  {
+    path: "/userProfile",
+    element: <UserProfile />,
   },
 ]);
