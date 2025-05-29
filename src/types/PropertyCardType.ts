f9423c0401443f79020d3bb9bb6cec228ebe5f84
AliArif5781 <aliairf92@gmail.com>
2025-05-29 21:42:49 +0500

    Add PropertyCardType.ts

---
+++ src/types/PropertyCardType.ts
@@ -0,0 +1,10 @@
+export interface Property {
+  id: string;
+  title: string;
+  address: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  imageUrl: string;
+  location: string;
+}
