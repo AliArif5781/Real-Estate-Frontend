1fa891d0a3a0b8c068f2573012188f0229011fe7
AliArif5781 <aliairf92@gmail.com>
2025-06-03 02:43:57 +0500

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,6 +7,7 @@ import { Aboutus } from "../pages/Aboutus";
 import NotFoundPage from "../pages/404Page";
 import { UserProfile } from "../pages/UserProfile";
 import { MainPage } from "../pages/MainPage";
+import PostPage from "../pages/PostPage";
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path: "mainPage",
         element: <MainPage />,
+      },
+      {
+        path: "postPage",
+        element: <PostPage />,
       },
     ],
   },
