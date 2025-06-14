18208c738d1364796cef2e5d3113d3b13e3cfe8a
AliArif5781 <aliairf92@gmail.com>
2025-06-14 16:41:22 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { useAppDispatch } from "./app/hook";
+import { useEffect } from "react";
+import { getUserData } from "./features/property/UserData";
 
-const App = () => {
+export const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        await dispatch(getUserData());
+        console.log("user data loaded");
+      } catch (error: any) {
+        return error?.response?.data || "Failed to Fetch data";
+      }
+    };
+    getData();
+  }, [dispatch]);
   return <RouterProvider router={router}></RouterProvider>;
 };
-
-export default App;
