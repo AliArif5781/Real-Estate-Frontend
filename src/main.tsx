617fcc19949ce231d576d4e0a25054905cecfe4f
AliArif5781 <aliairf92@gmail.com>
2025-06-14 16:41:24 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { App } from "./App.tsx";
 import "./assets/sass/main.scss";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./app/store/PropertyStore.ts";
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-    <Toaster />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+      <Toaster />
+    </StrictMode>
+  </Provider>
 );
