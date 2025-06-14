f57fdb7057fb831bc4fffa37a1123a4e68e84be8
AliArif5781 <aliairf92@gmail.com>
2025-06-14 16:41:23 +0500

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,43 +1,40 @@
-import type React from "react";
-import Button from "./Button";
+import React from "react";
 
 type FormStatus = "idle" | "submitting" | "success" | "error";
 
 interface FormProps {
   children: React.ReactNode;
-  className: string;
-  status: FormStatus;
-  errorMessage: string;
-  submitText: string;
-  successMessage: string;
+  className?: string;
+  status?: FormStatus;
+  errorMessage?: string;
+  // submitText?: string;
+  successMessage?: string;
   onSubmit: (e: React.FormEvent) => void;
 }
+
 export const Form = ({
-  children,
   className = "",
   errorMessage = "Something went wrong",
-  submitText = "submit",
+  // submitText = "Submit",
   successMessage = "Success!",
   status = "idle",
   onSubmit,
+  children,
 }: FormProps) => {
   return (
-    <>
-      <form action="" noValidate onSubmit={onSubmit}>
-        <fieldset
-          className={`space-y-4 ${className}`}
-          disabled={status === "submitting"}
-        >
-          {children}
-          {status === "error" && (
-            <div className="text-red-600 py-2">{errorMessage}</div>
-          )}
-
-          {status === "success" && (
-            <div className="text-green-600 py-2">{successMessage}</div>
-          )}
-        </fieldset>
-      </form>
-    </>
+    <form onSubmit={onSubmit}>
+      <fieldset
+        className={`space-y-4 ${className}`}
+        disabled={status === "submitting"}
+      >
+        {children}
+        {status === "error" && (
+          <div className="text-red-600 py-2">{errorMessage}</div>
+        )}
+        {status === "success" && (
+          <div className="text-green-600 py-2">{successMessage}</div>
+        )}
+      </fieldset>
+    </form>
   );
 };
