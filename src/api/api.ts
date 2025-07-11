9aeab552dd3efa2019a7f393668489e4d5da517e
AliArif5781 <aliairf92@gmail.com>
2025-07-12 01:59:52 +0500

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/user",
-  // baseURL: "https://real-estate-backend-ivory.vercel.app/api/user",
+  // baseURL: "http://localhost:3000/api/user",
+  baseURL: "https://real-estate-backend-ivory.vercel.app/api/user",
   withCredentials: true,
 });
 
 export const searchApiPost = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://real-estate-backend-ivory.vercel.app",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://real-estate-backend-ivory.vercel.app",
   withCredentials: true,
 });
