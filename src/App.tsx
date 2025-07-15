12a2f1594ef6cdf75bf39f44768df37e2ca9809b
AliArif5781 <aliairf92@gmail.com>
2025-07-15 22:20:11 +0500

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { useAppDispatch } from "./app/hook";
+import { useAppDispatch, useAppSelector } from "./app/hook";
 import { useEffect } from "react";
 import { getUserData } from "./features/property/UserData";
 
 export const App = () => {
   const dispatch = useAppDispatch();
+
+  const { initialized } = useAppSelector((state) => state.userData);
+  // console.log(data, "app.tsx");
   useEffect(() => {
     const getData = async () => {
       try {
@@ -17,5 +20,11 @@ export const App = () => {
     };
     getData();
   }, [dispatch]);
+
+  // protected-Route
+  if (!initialized) {
+    return <div>Loading application...</div>;
+  }
+
   return <RouterProvider router={router}></RouterProvider>;
 };
