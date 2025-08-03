29c87f85d2db346dac12addfa05bd5fd4a8d19d4
AliArif5781 <aliairf92@gmail.com>
2025-08-03 16:44:05 +0500

    Add Test.tsx

---
+++ src/pages/Test.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../app/hook";
+import { soldPropertiesData } from "../features/property/soldPropertySlice";
+
+const Test = () => {
+  return (
+    <div className="h-dvh flex justify-center items-center animate-pulse transition-all">
+      Loading...
+    </div>
+  );
+};
+
+export default Test;
