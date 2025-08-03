1ae354be4d29b6976d6ce09fab0162e71cd68d61
AliArif5781 <aliairf92@gmail.com>
2025-08-03 16:44:01 +0500

    Add CountSkeleton.tsx

---
+++ src/pages/AdminDashboard/CountSkeleton.tsx
@@ -0,0 +1,7 @@
+export const CountSkeleton = () => {
+  return (
+    <div className="hidden md:block text-left min-w-0 max-w-[140px] sm:max-w-[180px]">
+      <div className="h-6 w-[25px] mb-1 bg-gray-200 rounded-md animate-pulse"></div>
+    </div>
+  );
+};
