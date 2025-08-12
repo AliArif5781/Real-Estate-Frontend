6dbb582beb00d22714159f5d63da303ce036c054
AliArif5781 <aliairf92@gmail.com>
2025-08-12 09:14:13 +0500

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:3000/api/user",
-  baseURL: "real-estate-backend-brown-nine.vercel.app/api/user",
+  baseURL: "https://real-estate-backend-brown-nine.vercel.app/api/user",
   withCredentials: true,
 });
 
 export const searchApiPost = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "real-estate-backend-brown-nine.vercel.app",
+  baseURL: "https://real-estate-backend-brown-nine.vercel.app",
   withCredentials: true,
 });
