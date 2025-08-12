b9187ade3583d4ac83befe8a4507714de2b5f405
AliArif5781 <aliairf92@gmail.com>
2025-08-12 08:05:43 +0500

    Add PropertyNotFound.tsx

---
+++ src/pages/PropertyNotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PropertyNotFound = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl font-medium text-gray-500 animate-pulse bg-gradient-to-tr">
+      PropertyNotFound...
+    </div>
+  );
+};
+
+export default PropertyNotFound;
