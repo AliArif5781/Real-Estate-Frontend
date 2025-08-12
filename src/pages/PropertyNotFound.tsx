d29595b2355f4c44c4aa2c5766f309daecb97e8d
AliArif5781 <aliairf92@gmail.com>
2025-08-12 09:01:23 +0500

    Update PropertyNotFound.tsx

--- src/pages/PropertyNotFound.tsx
+++ src/pages/PropertyNotFound.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const PropertyNotFound = () => {
   return (
     <div className="min-h-screen flex items-center justify-center text-2xl font-medium text-gray-500 animate-pulse bg-gradient-to-tr">
